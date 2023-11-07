1d88d3fcbb3bfcc26ac56730e5d62b4f7ca0c62e
Hannele Ruiz <justlemoncl@gmail.com>
2023-11-07 11:31:24 -0300

    Added command to get the number of players

--- commands.ts
+++ commands.ts
@@ -6,4 +6,21 @@ type Command = CreateApplicationCommandOptions & {
 
 const commands: Map<string, Command> = new Map<string, Command>();
 
+async function commandPlayer(interaction: CommandInteraction) {
+    setImmediate(async () => {
+        const count = GetNumPlayerIndices();
+        const total = GetConvarInt("sv_maxclients", -1);
+    
+        await interaction.createMessage({
+            content: `There are ${count} players connected (max ${total})`,
+        });
+    });
+}
+commands.set("players", {
+    type: ApplicationCommandTypes.CHAT_INPUT,
+    name: "players",
+    description: "Lists the players on the server",
+    handler: commandPlayer,
+});
+
 export { commands };
