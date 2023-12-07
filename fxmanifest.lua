ddd546854845e6ca48b9382d970e4fa4d852e872
Hannele Ruiz <justlemoncl@gmail.com>
2023-12-07 06:27:24 -0300

    Added more metadata fields

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version "cerulean"
 games { "gta5", "rdr3" }
 
+author "Lemon"
+description "A simple Discord to FiveM/RedM bridge"
+version "1.0.0"
+
 server_script "server.js"
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
