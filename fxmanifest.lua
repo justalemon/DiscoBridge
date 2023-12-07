2d9483baf392cfedfcfd6c09765a849d8cfb5bc4
Hannele Ruiz <justlemoncl@gmail.com>
2023-12-07 05:33:20 -0300

    Added missing rdr3 warning
    Why is this still needed lol

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,3 +2,5 @@ fx_version "cerulean"
 games { "gta5", "rdr3" }
 
 server_script "server.js"
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
