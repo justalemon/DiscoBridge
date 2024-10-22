925cff20fe93c0aaa6599ea070037a9500934d3a
Hannele Ruiz <justlemoncl@gmail.com>
2024-10-22 01:24:50 -0300

    Added Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    server: "./sv_main.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    iife: false,
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        "fxmanifest.lua",
+      ],
+    }),
+  ],
+};
