1b13e7530bc70de30bc4dfa3124a0fe5cbd6f815
Hannele Ruiz <justlemoncl@gmail.com>
2024-10-23 00:55:42 -0300

    Added Discord based whitelisting

--- server.ts
+++ server.ts
@@ -1,7 +1,48 @@
 import { Discord } from "./discord/client";
+import { Deferrals, SetKickReason } from "./fxserver/types";
 
 const Delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 let discord: Discord | null = null;
+
+const whitelist = GetConvarInt("discord_whitelist", 0) != 0;
+
+async function handleJoinWhitelist(playerName: string, setKickReason: SetKickReason, deferrals: Deferrals) {
+    const src = source;
+
+    if (!whitelist) {
+        return;
+    }
+
+    deferrals.defer();
+    deferrals.update("Fetching your Discord ID...");
+
+    await Delay(1);
+
+    const snowflake = GetPlayerIdentifierByType(src.toString(), "discord");
+
+    if (snowflake == null) {
+        deferrals.done("Your Discord ID is not available. Please open the Discord app and restart the game.");
+        return;
+    }
+    
+    deferrals.update("Fetching your Discord details...");
+
+    const guild = await discord?.getGuild(GetConvar("discord_guild", ""));
+
+    if (guild == null) {
+        deferrals.done("The Discord server is not available, please contact a staff member.");
+        return;
+    }
+
+    const member = await discord?.getMember(guild.id, snowflake.replace("discord:", ""));
+
+    if (member == null) {
+        deferrals.done("Please join our Discord server so you can play online.");
+        return;
+    }
+
+    deferrals.done();
+}
 
 function init() {
     const token = GetConvar("discord_token", "");
@@ -13,5 +54,7 @@ function init() {
     }
 
     discord = new Discord(token);
+
+    on("playerConnecting", handleJoinWhitelist);
 }
 init();
