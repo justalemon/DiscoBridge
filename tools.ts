73d6a7258938ef1027b22ac58d0c1d87a5f875b3
Hannele Ruiz <justlemoncl@gmail.com>
2024-10-24 03:41:30 -0300

    Prefix the debug messages with Debug

--- tools.ts
+++ tools.ts
@@ -1,5 +1,5 @@
 export function debug(...objs: any[]) {
     if (GetConvarInt("lemon_debug", 0) !== 0 || GetConvarInt("lemon_" + GetCurrentResourceName() + "_debug", 0) !== 0) {
-        console.log(...objs);
+        console.log("Debug: ", ...objs);
     }
 }
