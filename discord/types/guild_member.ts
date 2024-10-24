ef89c8cebee394aa33d0155fc29664a7e63eafc5
Hannele Ruiz <justlemoncl@gmail.com>
2024-10-24 20:03:11 -0300

    Fixed DiscordGuildMember type

--- discord/types/guild_member.ts
+++ discord/types/guild_member.ts
@@ -2,18 +2,21 @@ import { DiscordAvatarDecorationData } from "./avatar_decoration_data";
 import { DiscordUser } from "./user";
 
 export interface DiscordGuildMember {
-    user: DiscordUser,
-    nick: string | null,
-    avatar: string | null,
-    banner: string | null,
+    user?: DiscordUser,
+    nick?: string | null,
+    avatar?: string | null,
+    banner?: string | null,
     roles: string[],
     joined_at: string,
-    premium_since: string | null,
+    premium_since?: string | null,
     deaf: boolean,
-    muttte: boolean,
+    mute: boolean,
     flags: number,
-    pending: boolean,
-    permissions: string,
-    communication_disabled_until: string | null,
-    avatar_decoration_data: DiscordAvatarDecorationData
+    pending?: boolean,
+    permissions?: string,
+    communication_disabled_until?: string | null,
+    avatar_decoration_data?: DiscordAvatarDecorationData,
+
+    // Found in Gateway messages, not documented somehow
+    guild_id?: string
 }
