e278ac5089ff3facc7604714a56b6f1dae48f01e
Hannele Ruiz <justlemoncl@gmail.com>
2024-10-25 00:16:25 -0300

    Fixed double sapce in debug messages

--- tools.ts
+++ tools.ts
@@ -1,6 +1,6 @@
 export function debug(...objs: any[]) {
     if (GetConvarInt("lemon_debug", 0) !== 0 || GetConvarInt("lemon_" + GetCurrentResourceName() + "_debug", 0) !== 0) {
-        console.log("Debug: ", ...objs);
+        console.log("Debug:", ...objs);
     }
 }
 
